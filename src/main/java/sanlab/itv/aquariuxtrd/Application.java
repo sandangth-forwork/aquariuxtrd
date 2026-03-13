41f31db1c5f82a57684f8dc3ed4ac54303faf6a0
San Dang <dangthaisan@hotmail.com>
2026-03-13 15:52:59 +0700

    Initial Commit

---
+++ src/main/java/sanlab/itv/aquariuxtrd/Application.java
@@ -0,0 +1,13 @@
+package sanlab.itv.aquariuxtrd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/sanlab/itv/aquariuxtrd/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sanlab.itv.aquariuxtrd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
